627f3b95c3feb1cbb4946376207383c7008e768b
pthushanth <pthushanth97@gmail.com>
2022-07-22 17:34:02 +0200

    Fix some import error

--- reactjs_u/src/pages/home/Home.jsx
+++ reactjs_u/src/pages/home/Home.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
+import Topbar from "../../components/topbar/Topbar";
 import "./home.css"
 
 export default function Home() {
   return (
     <>
-    {/* <Topbar /> */}
+    <Topbar />
     <div className="homeContainer">
       <Sidebar />
       <Feed/>
