23c0e387b59db24c7c779b5cfcbb14bfa92e3f9d
pthushanth <pthushanth97@gmail.com>
2022-07-25 19:53:34 +0200

    Generate initial avatar from username

--- reactjs_u/src/components/closeFriend/CloseFriend.jsx
+++ reactjs_u/src/components/closeFriend/CloseFriend.jsx
@@ -1,8 +1,11 @@
+import { Avatar } from "@mui/material";
+import CustomAvatar from "../avatar/CustomAvatar";
 import "./closeFriend.css";
 export default function CloseFriend({user}) {
   return (
     <li className="sidebarFriend">
-      <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
+      <CustomAvatar name={user.username} />
+      {/* <img className="sidebarFriendImg" src={user.profilePicture} alt="" /> */}
       <span className="sidebarFriendName">{user.username}</span>
     </li>
   );
