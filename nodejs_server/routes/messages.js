2a5455fc6c5c9bf1576d346940129db0c2d086f8
pthushanth <pthushanth97@gmail.com>
2022-07-27 23:40:12 +0200

    send message to friend

--- nodejs_server/routes/messages.js
+++ nodejs_server/routes/messages.js
@@ -3,9 +3,10 @@ import {
   getAllMessages,
 } from "../controllers/messageController.js";
 import { Router } from "express";
+import auth from "../middlewares/auth.js";
 
 const router = Router();
-router.post("/addMessage/", addMessage);
-router.post("/allMessages/", getAllMessages);
+router.post("/addMessage/", auth, addMessage);
+router.post("/allMessages/", auth, getAllMessages);
 
 export default router;
