802f421825d54f239ce150e6cf7017f84ccd878d
pthushanth <pthushanth97@gmail.com>
2022-07-27 23:58:50 +0200

    admin

--- reactjs_u/src/Admin/pages/userList/UserList.jsx
+++ reactjs_u/src/Admin/pages/userList/UserList.jsx
@@ -3,19 +3,52 @@ import { DataGrid } from "@mui/x-data-grid";
 import { DeleteOutline } from "@mui/icons-material";
 import { userRows } from "../../dummyData";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { getAllUsersRoute } from "../../../utils/ApiRoutes";
+import { AuthContext } from "../../../context/Auth";
+import { List, ListItem } from "@mui/material";
 
 export default function UserList() {
   const [data, setData] = useState(userRows);
 
+  const { token, user } = useContext(AuthContext);
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchDataJson() {
+      let contacts = null;
+      return await axios.get(getAllUsersRoute, {
+          headers: {
+            Authorization: `token ${token}`,
+          },
+        })
+        .then((res) => {
+          contacts = res.data;
+          return contacts;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    if (user !== null) {
+      fetchDataJson().then((data) => {
+        setUsers(data);
+        console.log(data);
+      });
+    }
+  }, [user]);
+
   const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
+    setData(users.filter((item) => item.id !== id));
   };
   
   const columns = [
-    { field: "id", headerName: "ID", width: 90 },
+    { field: "_id", headerName: "ID", width: 90 },
     {
-      field: "user",
+      field: "username",
       headerName: "User",
       width: 200,
       renderCell: (params) => {
@@ -28,16 +61,6 @@ export default function UserList() {
       },
     },
     { field: "email", headerName: "Email", width: 200 },
-    {
-      field: "status",
-      headerName: "Status",
-      width: 120,
-    },
-    {
-      field: "transaction",
-      headerName: "Transaction Volume",
-      width: 160,
-    },
     {
       field: "action",
       headerName: "Action",
@@ -59,14 +82,36 @@ export default function UserList() {
   ];
 
   return (
+    
     <div className="userList" style={{height:400, width: '100%'}}>
+      {/* {users.length > 0 &&       
       <DataGrid
-        rows={data}
+        rows={users}
         disableSelectionOnClick
         columns={columns}
         pageSize={8}
         checkboxSelection
-      />
+        />
+      } */}
+
+      <List>
+        {users.map((user) => (
+          <ListItem key={user.id}>
+            <img src={user.avatar} alt="" />
+            <div className="userListUser">
+              <h3>{user.username}</h3>
+              <p>{user.email}</p>
+            </div>
+            <div className="userListAction">
+              <Link to={"/admin/user/" + user.id}>
+                <button className="userListEdit">Supprimer</button>
+              </Link>
+            </div>
+          </ListItem>
+        ))}
+      </List>
+
+      
     </div>
   );
 }
